75c3c85a2f07e249ad6d8404b936be30a35c873b
William Matos <wiusk8@gmail.com>
2024-04-29 02:42:46 -0300

    :lipstick: feat/create login page

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+  
+  return (
+   <div className='header-div'>
+        <div className='content'>
+          <div className='content-main-text'>
+            <div className='content-div-button-gitproject'>
+              <button className='button-gitproject'>GITHUB PROJECT</button>
+            </div>
+            <div className='content-text-title'>
+              <p className='title-pag'>PROJECT ANIME'S</p>
+              <button className='button-getstarted'>GET STARTED</button>
+            </div>
+          </div>
+
+          <div className='images-header-pag'>
+            <div className='images-content tree-divs'>
+                <div className='image-pag one'></div>
+                <div className='image-pag two'></div>
+                <div className='image-pag tree'></div>
+            </div>
+            <div className='images-content two-divs'>
+                <div className='image-pag four'></div>
+                <div className='image-pag five'></div>
+            </div>
+          </div>
+        </div>
+   </div>
+  )
+}
+
+export default App
