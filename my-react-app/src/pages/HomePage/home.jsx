ffb27c996c7aff9d61cf3a6fd1f05ecded25fbf0
William Matos <wiusk8@gmail.com>
2024-05-06 21:05:07 -0300

    fix/time load page

--- my-react-app/src/pages/HomePage/home.jsx
+++ my-react-app/src/pages/HomePage/home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
      setLoading(false); 
-    }, 4000)
+    }, 3000)
 
     return () => clearTimeout(timer);
 
